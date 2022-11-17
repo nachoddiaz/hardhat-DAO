633a25afe2fbf5a2f451ed1517406babf9227c34
nachoddiaz <nachodiazutr@gmail.com>
2022-11-17 17:54:56 +0100

    deploy script for governance token done

--- deploy/01-deploy-govToken.ts
+++ deploy/01-deploy-govToken.ts
@@ -2,6 +2,7 @@ import {HardhatRuntimeEnvironment} from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/dist/types"
 import networkConfig from "../helper-hardhat-config"
 import {verify} from "../utils/verify"
+import { ethers } from "hardhat";
 
 const deployGovToken: DeployFunction = async function(
     hre: HardhatRuntimeEnvironment
@@ -9,20 +10,33 @@ const deployGovToken: DeployFunction = async function(
         const{getNamedAccounts, deployments, network} = hre;
         const { deploy, log } = deployments
         const { deployer } = await getNamedAccounts()
-        log("Deploying Governance token")
         const chainId = network.config.chainId
 
         const governanceToken = await deploy("GovToken", {
             from: deployer,
             args: [],
             log: true,
-            waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
+            /* waitConfirmations: networkConfig[network.name].blockConfirmations || 1, */
         })
+        log(`Governance token contract deployed at address ${governanceToken.address}`)
         if (chainId != 31337 && process.env.ETHERSCAN_API_KEY) {
-            log("Verificando....")
             await verify(governanceToken.address, arguments)
+            log("Contract Verified")
         }
+
+        await delegate(governanceToken.address, deployer)
+        console.log(`votes delegated to ${deployer}`)
     };
 
+    const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
+        const governanceToken = await ethers.getContractAt("GovToken", governanceTokenAddress)
+        /* const TxResponse = await governanceToken.delegate(delegatedAccount) */
+        const transactionResponse = await governanceToken.delegate(delegatedAccount)
+        await transactionResponse.wait(1)
+        console.log(`Checkpoints ${await governanceToken.numCheckpoints(delegatedAccount)}`)
+    } 
 
-   export default deployGovToken ;
+
+
+   export default deployGovToken;
+
